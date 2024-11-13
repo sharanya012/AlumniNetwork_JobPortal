1adb2261f962312f7cbee218b65cb70adbff7d42
Samiksha M <123168624+sam1ksha@users.noreply.github.com>
2024-11-13 17:03:45 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'M@halakshmi234',
+    password: process.env.DB_PASSWORD || 'ShArAnYa123!@#',
     database: process.env.DB_NAME || 'alumni_portal',
     waitForConnections: true,
     connectionLimit: 10,
